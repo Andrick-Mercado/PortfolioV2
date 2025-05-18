0af00c2e4ab121701e2a3ba329a9f9daf2f9768c
Andrick Mercado <andrickmercado17@gmail.com>
2025-05-18 16:18:25 -0700

    Deleting files

--- src/PersonalPortfolio.Blazor/Pages/AllCardsPage.razor.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Components;
-
-namespace PersonalPortfolio.Blazor.Pages;
-
-public partial class AllCardsPage
-{
-    [Parameter]
-    public string ClientRouteName { get; set; } = default!;
-}
